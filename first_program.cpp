e4788c62774477cf451ed68594272d49e91104e6
Dinesh Ravindran <dravindran9692@floridapoly.edu>
2025-08-20 18:25:20 +0000

    aaa

--- first_program.cpp
+++ first_program.cpp
@@ -10,10 +10,12 @@ The first C++ program
 
 int main(void)
 {
-    std::cout
+    std::cout << "Hello COP3337!" << std::endl;
 
     //std = standard namespace
     //:: = scope resolution operator
     //cout = console output (object of an iostream class)
+    //<< stream insertion operator
+    //endl = end of line
     return 0;
 }
