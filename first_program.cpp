f8d7b4a862a3863fd8a8c0a53b324a00f780a1e7
Dinesh Ravindran <dravindran9692@floridapoly.edu>
2025-08-20 18:23:10 +0000

    aaaaaaaaaaaaaaa

---
+++ first_program.cpp
@@ -0,0 +1,19 @@
+/*
+8/20/25
+The first C++ program
+
+*/
+
+#include <iostream>
+
+// Create a main function
+
+int main(void)
+{
+    std::cout
+
+    //std = standard namespace
+    //:: = scope resolution operator
+    //cout = console output (object of an iostream class)
+    return 0;
+}
