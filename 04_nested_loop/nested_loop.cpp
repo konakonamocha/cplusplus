5875893f19d94f33f3252988619d2903e06735d4
Dinesh Ravindran <dravindran9692@floridapoly.edu>
2025-08-27 18:52:35 +0000

    files to be silly to

---
+++ 04_nested_loop/nested_loop.cpp
@@ -0,0 +1,23 @@
+//this code does not cmoplile lmaoooooooo
+
+#include <iostream>
+
+int main(void)
+{
+    int n;
+    std::cin >> n;
+    // n n-1 n-2... 1 2 3....n
+
+    for(int j = 0; j < n; j++)
+    { 
+        std::cout << i << ' ';
+    }
+    for(int i = n; i > 0; i--)
+    {
+        std::cout << i << ' ';
+    }
+    for(int i = 2; i > 0; i++)
+    {
+        std::cout << i << ' ';
+    }
+}
---
+++ 05_reference/reference.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main(void)
+{
+    std::string s1 = "hello";
+
+    title(s[1]);
+
+    return 0;
+}
+
+void title(std::string s)
+{
+    s[0] = to_upper(s[0]);
+
+    std::cout << s << std::endl;
+}
