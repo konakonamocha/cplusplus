ff3a2c3dbb64d43d988670f5d4997354fb463bc3
Dinesh Ravindran <dravindran9692@floridapoly.edu>
2025-09-05 18:07:59 +0000

    noooootes

---
+++ notes/8-27.cpp
@@ -0,0 +1,14 @@
+/*
+with rand(), it has a limited range and poor randomness quality
+with <random>, it has high quality randomness and flexible distribution
+*/
+
+/*
+continue breaks out of an iteration
+break breaks out of the entire loop
+*/
+
+//function prototypes used to verify if function is valid
+
+/*
+*/
