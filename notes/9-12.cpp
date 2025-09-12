8f5d4dec8306ae6a57d80b838cbdf01fea85c114
Dinesh Ravindran <dravindran9692@floridapoly.edu>
2025-09-12 18:49:05 +0000

    as

---
+++ notes/9-12.cpp
@@ -0,0 +1,15 @@
+/*
+vector = dynamic array, stores elements of a single data type 
+* is used to return the value of the objec tthat it points to
+
+std::cout << *yPtr // this prints the value of y
+    using it like this derefrences the pointer
+
+always initialize pointers to prevent errrors
+dereferenceing a pointer that is not initialized could cause a fatal runtime error or point to the wrong data
+cpp has a nullptr (Null Pointer) (dont ask me what tha tis)
+
+the const qualifier tells compiler that the value should not be modified
+
+functions CAN return a pointer
+*/
